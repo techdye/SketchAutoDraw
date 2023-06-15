d7b9ee166412e3b7ccc90815b39ed8dcbad5e88e
tistirone <tistirone@gmail.com>
2023-06-15 18:04:06 +0200

    Commit

---
+++ backend/image.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import requests
+from io import BytesIO
+
+
+def _get_image_online(url: str) -> Image:
+    """
+    Getting an image online.
+    :param url: The url.
+    :return: Image.
+    """
+    response = requests.get(url)
+    return Image.open(BytesIO(response.content))
+
+
+if __name__ == "__main__":
+    _get_image_online("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80").show()
