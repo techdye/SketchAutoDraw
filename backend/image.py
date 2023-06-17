816fefd4555bfb0f457820be638126b70e12e337
tistirone <tistirone@gmail.com>
2023-06-17 08:13:55 +0200

    Delete prints

--- backend/image.py
+++ backend/image.py
@@ -2,6 +2,8 @@ from PIL import Image
 import requests
 from io import BytesIO
 
+import logging
+
 url: str = "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
 
 
@@ -12,6 +14,8 @@ def _get_image_online(url: str) -> Image:
     :return: Image.
     """
     response = requests.get(url)
+
+    logging.debug(f"Getting an image online : {url}")
     return Image.open(BytesIO(response.content))
 
 
@@ -19,12 +23,16 @@ def _resize(image: Image, pos1: list[int], pos2: list[int]):
     w = abs(pos1[0] - pos2[0])
     h = abs(pos1[1] - pos2[1])
 
+    print(w)
+    print(h)
+
+    logging.debug(f"Resizing an image : {w}, {h}")
     return image.resize((w, h))
 
 
 if __name__ == "__main__":
     image = _get_image_online(url)
 
-    image = _resize(image, [500, 100], [200, 800])
+    image = _resize(image, [-100, -100], [100, -100])
 
     image.show()
--- tests/test_image.py
+++ tests/test_image.py
@@ -1,2 +1,59 @@
-def test__get_image_online():
-    assert False
+import PIL
+import pytest
+import requests
+
+import backend.image
+
+
+@pytest.fixture
+def image_online():
+    return backend.image._get_image_online("https://assets.stickpng.com/images/5848152fcef1014c0b5e4967.png")
+
+
+def test__get_image_online(image_online):
+    assert image_online
+
+
+def test__get_image_online_missing_schema():
+    with pytest.raises(requests.exceptions.MissingSchema):
+        backend.image._get_image_online("adadadadada")
+
+
+def test__get_image_online_bad_url():
+    with pytest.raises(PIL.UnidentifiedImageError):
+        assert backend.image._get_image_online("https://assets.stickpng.com/images/itsaveryverycooltest.png")
+
+
+def test__resize_positive(image_online):
+    image = backend.image._resize(image_online, [100, 100], [200, 200])
+
+    w, h = image.size
+
+    assert w == 100
+    assert h == 100
+
+
+def test__resize_negative(image_online):
+    image = backend.image._resize(image_online, [-100, -100], [-200, -200])
+
+    w, h = image.size
+
+    assert w == 100
+    assert h == 100
+
+
+def test__resize_inversed(image_online):
+    image = backend.image._resize(image_online, [200, 200], [100, 100])
+
+    w, h = image.size
+
+    assert w == 100
+    assert h == 100
+
+
+def test__resize_equality(image_online):
+    with pytest.raises(ValueError):
+        backend.image._resize(image_online, [200, 50], [200, 100])
+
+    with pytest.raises(ValueError):
+        backend.image._resize(image_online, [200, 100], [200, 100])
