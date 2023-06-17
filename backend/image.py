f8e4de5f63e009b64c8180e4ce7de3c881772da8
tistirone <tistirone@gmail.com>
2023-06-17 07:45:22 +0200

    Commit

--- backend/image.py
+++ backend/image.py
@@ -2,6 +2,8 @@ from PIL import Image
 import requests
 from io import BytesIO
 
+url: str = "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+
 
 def _get_image_online(url: str) -> Image:
     """
@@ -14,4 +16,6 @@ def _get_image_online(url: str) -> Image:
 
 
 if __name__ == "__main__":
-    _get_image_online("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80").show()
+    image = _get_image_online(url).resize((256, 256))
+
+    image.show()
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
-### To-Do : 
-- Récuppérer une image d'internet
+### To-Do :
 - Recadrer bien l'image
 - Diviser son nombre de pixel par 5
 - Récupérer les pixels sous forme de colonnes (tableau dans un tableau)
