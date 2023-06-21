717403f2fc376773a4fe48603adbbf9a9a61ddd0
tistirone <tistirone@gmail.com>
2023-06-21 15:14:41 +0200

    Modification for Mouse click position

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -21,7 +21,7 @@ def colors_near():
 def test__mouse_click_position():
     screen_width, screen_height = pyautogui.size()
 
-    sleep(1.5)
+    sleep(0.5)
 
     backend.draw._mouse_click_position((screen_width // 2, screen_height // 2))
     assert pyautogui.position() == (screen_width // 2, screen_height // 2)
